dc6fcb238f2fcb4f24f3d1dc7321c150b10681a7
Leandro <leohdc13@gmail.com>
2025-08-24 22:18:43 -0300

    primeiro commit

---
+++ #04.py
@@ -0,0 +1,11 @@
+# nome = input('Qual o seu nome?')
+# print('Bem vindo', nome, '!')
+
+# dia = input('Que dia você nasceu?')
+# mes = input('De qual mes?')
+# ano = input('E qual ano?')
+# print('Você nasceu no dia', dia, 'de', mes, 'de', ano,". Correto?")
+
+numero1 = int(input('Digite um numero '))
+numero2 = int(input('Digite um numero '))
+print(numero1 + numero2)
