57e87519c669db677a3bf1ef9cfc1e3edf82ba3a
leandro-henrique09 <leandrohdc09@gmail.com>
2025-08-25 22:28:42 -0300

    ajuste de arquivos

---
+++ aula06.py
@@ -0,0 +1,18 @@
+# minha versao minuto 18:29 da aula #06
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite mais um numero: '))
+s = n1 + n2
+print('A soma entre {} e {} é igual a {}'.format(n1, n2, s))
+
+# reposta do video
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro: '))
+s = n1 + n2
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
+
+# TIPOS PRIMITIVOS
+# str, bool, int, float
+# str = serve para declarar como string
+# bool = serve para ver se é falso ou true
+# int = serve para declarar como numero inteiro
+# float = serve para declarar como numero flutuante ou numero 'quebrado'
