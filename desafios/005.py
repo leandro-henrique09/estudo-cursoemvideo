68605886c4c375239f8ebcd35af8b0ee76b3a0e8
leandro-henrique09 <leandrohdc09@gmail.com>
2025-08-26 00:58:55 -0300

    aula07 e desafios 05 e 06

---
+++ desafios/005.py
@@ -0,0 +1,14 @@
+# minha versão
+numero = int(input('Digite um número: '))
+antecessor = numero - 1
+sucessor = numero + 1
+
+print('O numero {} tem o {} como antecessor e o {} como sucessor!'.format(numero, antecessor, sucessor))
+
+# resposta do video
+n = int(input('Digite um número: '))
+a = n - 1
+s = n + 2
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, a, s))
+# outra maneira
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n,(n-1),(n+2)))
---
+++ desafios/006.py
@@ -0,0 +1,11 @@
+# minha versão
+numero = int(input('Digite um número: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero**(1/2)
+
+print('O dobro de {} é {}'.format(numero, dobro))
+print('O triplo de {} é {}'.format(numero, triplo))
+print('A raiz de {} é {}'.format(numero,raiz))
+
+# não coloquei a resposta do video pq fui dormir
