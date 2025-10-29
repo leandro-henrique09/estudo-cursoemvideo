59a6c18661227a677b6b45a83bdb20d75a260a31
leandro-henrique09 <leandrohdc09@gmail.com>
2025-10-29 01:12:53 -0300

    desafio 85

--- desafios/85.py
+++ desafios/85.py
@@ -1,3 +1,9 @@
-lista = []
+lista = [[],[]]
 for i in range(1,8):
     num = int(input(f'Digite o {i}° número: '))
+    if num % 2 == 0:
+        lista[0].append(num)
+    else:
+        lista[1].append(num)
+print(f'Os valores pares digitados foram:{sorted(lista[0])}')
+print(f'Os valores impares digitados foram:{sorted(lista[1])}')
