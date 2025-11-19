14e1cbc11571e9c799c411e18aa3fed40d7a1bb4
leandro-henrique09 <leandrohdc09@gmail.com>
2025-11-18 23:02:44 -0300

    desafio 107

---
+++ desafios/107.py
@@ -0,0 +1,7 @@
+from uteis import moeda
+
+valor = int(input('Digite um preço: '))
+moeda.metade(valor)
+moeda.dobro(valor)
+moeda.aumentar(valor)
+moeda.diminuir(valor)
---
+++ desafios/uteis/moeda.py
@@ -0,0 +1,12 @@
+def aumentar(p):
+    print(f'Aumentando 10%, temos R${p + p * 10/100}')
+
+def metade(p):
+    print(f'A metade de {p} é R${p/2}')
+
+def dobro(p):
+    print(f'O dobro de {p} é R${p*2}')
+
+def diminuir(p):
+    print(f'Diminuindo 10% de {p} teremos {p - p * 10/100}')
+
